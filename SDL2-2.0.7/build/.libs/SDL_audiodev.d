8c35e0bad0a919a9f4d1255892714c382fab766c
Vincent CHARLES <vcharles@MacBook-Pro-de-Vincent.local>
2017-11-25 18:04:31 +0100

    copy pasta SDL

---
+++ SDL2-2.0.7/build/.libs/SDL_audiodev.d
@@ -0,0 +1,7 @@
+build/SDL_audiodev.lo: \
+  /Users/vcharles/Downloads/SDL2-2.0.7/src/audio/SDL_audiodev.c \
+  /Users/vcharles/Downloads/SDL2-2.0.7/src/audio/../SDL_internal.h \
+  /Users/vcharles/Downloads/SDL2-2.0.7/src/audio/../dynapi/SDL_dynapi.h \
+  /Users/vcharles/Downloads/SDL2-2.0.7/src/audio/../dynapi/SDL_dynapi_overrides.h \
+  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
+  include/close_code.h
