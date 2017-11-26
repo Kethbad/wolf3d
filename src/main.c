c67a58a7a0ac185ebbbd4a72a463a9cd218dc2cd
Utilisateur invité <Guest@MacBook-Pro-de-Vincent-CHARLES.local>
2017-11-26 12:12:18 +0100

    test

--- src/main.c
+++ src/main.c
@@ -1,23 +1,42 @@
 #include "wolf3d.h"
+int main(int argc, char* argv[]) {
 
-int     main(int argc, char** argv) {
-    SDL_Window*     screen;
+    SDL_Window *window;                    // Declare a pointer
 
-    OpenCLInit();
+    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
 
-    if (!SDL_Init(SDL_INIT_EVERYTHING)) {
-        printf("SDL is running.\n");
-        SDL_CreateWindow(
-            "mon super test",
-            SDL_WINDOWPOS_UNDEFINED, 
-            SDL_WINDOWPOS_UNDEFINED, 
-            640, 480,
-            SDL_WINDOW_FULLSCREEN | SDL_WINDOW_OPENGL
-        );
-        SDL_Quit();
+    // Create an application window with the following settings:
+    window = SDL_CreateWindow(
+        "An SDL2 window",                  // window title
+        SDL_WINDOWPOS_UNDEFINED,           // initial x position
+        SDL_WINDOWPOS_UNDEFINED,           // initial y position
+        640,                               // width, in pixels
+        480,                               // height, in pixels
+        SDL_WINDOW_FULLSCREEN | SDL_WINDOW_OPENGL                  // flags - see below
+    );
+
+    // Check that the window was successfully created
+    if (window == NULL) {
+        // In the case that the window could not be made...
+        printf("Could not create window: %s\n", SDL_GetError());
+        return 1;
     }
+    SDL_Renderer *ren = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    // if (ren == nullptr){
+	  //    SDL_DestroyWindow(win);
+	  //  //   std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+	  //      SDL_Quit();
+	  //       return 1;
+    //     }
 
+    // The window is open: could enter program loop here (see SDL_PollEvent())
+
+    SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+
+    // Close and destroy the window
+    SDL_DestroyWindow(window);
+
+    // Clean ups
+    SDL_Quit();
     return 0;
-    (void)argc;
-    (void)argv;
 }
